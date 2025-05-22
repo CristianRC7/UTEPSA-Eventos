1b621d7e8dbfa12b6c0b9b62226079fcca10da4d
crist <cristian25ramirezrc@gmail.com>
2025-05-22 12:52:40 -0400

    variables de entorno

--- UTEPSAEventos/src/utils/Config.ts
+++ UTEPSAEventos/src/utils/Config.ts
@@ -1,3 +1,4 @@
-//export const BASE_URL = 'https://urbacarsrl.org/server/src';
+import { BASE_URL as BASE_URL_ENV } from '@env';
 
-export const BASE_URL = 'http://10.40.25.212/UTEPSA-Eventos/server/src';
+
+export const BASE_URL = BASE_URL_ENV || 'http://10.40.25.2/UTEPSA-Eventos/server/src';
