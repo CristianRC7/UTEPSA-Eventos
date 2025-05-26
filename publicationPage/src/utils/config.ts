41da07fdcd5a82e0941d071cd7259a2c151b9118
crist <cristian25ramirezrc@gmail.com>
2025-05-26 10:22:20 -0400

    cambios

--- publicationPage/src/utils/config.ts
+++ publicationPage/src/utils/config.ts
@@ -1,3 +1,1 @@
-// Archivo de configuración para rutas y variables globales
-
-export const BASE_IMG_URL = import.meta.env.VITE_BASE_IMG_URL || 'http://localhost/UTEPSA-Eventos/server/src/'; 
+export const BASE_IMG_URL = import.meta.env.VITE_BASE_IMG_URL || 'http://localhost/UTEPSA-Eventos/server/src/';
