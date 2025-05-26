23567812638242436c88c55f2490d2ccb6e56338
crist <cristian25ramirezrc@gmail.com>
2025-05-26 10:26:00 -0400

    pruebas

--- publicationPage/src/utils/config.ts
+++ publicationPage/src/utils/config.ts
@@ -1,1 +1,2 @@
-export const BASE_IMG_URL = import.meta.env.VITE_BASE_IMG_URL || 'http://localhost/UTEPSA-Eventos/server/src/';
+//export const BASE_IMG_URL = import.meta.env.VITE_BASE_IMG_URL || 'http://localhost/UTEPSA-Eventos/server/src/';
+export const BASE_IMG_URL = import.meta.env.VITE_BASE_IMG_URL || 'https://urbacarsrl.org/server/src/';
