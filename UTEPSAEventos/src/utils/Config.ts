96a0d6978c42cdb856db9b6662cccb45758e5af2
crist <cristian25ramirezrc@gmail.com>
2025-05-29 01:51:58 -0400

    env

--- UTEPSAEventos/src/utils/Config.ts
+++ UTEPSAEventos/src/utils/Config.ts
@@ -1,3 +1,3 @@
-//import { BASE_URL as BASE_URL_ENV } from '@env';
-//export const BASE_URL = BASE_URL_ENV || 'http://10.40.25.212/UTEPSA-Eventos/server/src';
-export const BASE_URL = 'http://192.168.0.7/UTEPSA-Eventos/server/src';
+import { BASE_URL as BASE_URL_ENV } from '@env';
+export const BASE_URL = BASE_URL_ENV || 'http://10.40.25.212/UTEPSA-Eventos/server/src';
+//export const BASE_URL = 'http://192.168.0.7/UTEPSA-Eventos/server/src';
