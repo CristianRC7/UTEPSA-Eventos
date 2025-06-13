09891d9276701f9a67d1633766a7c03a9dfbc9d6
crist <cristian25ramirezrc@gmail.com>
2025-06-13 00:11:12 -0400

    cambio de servidor

--- UTEPSAEventos/src/utils/Config.ts
+++ UTEPSAEventos/src/utils/Config.ts
@@ -1,4 +1,4 @@
 import { BASE_URL as BASE_URL_ENV } from '@env';
 export const BASE_URL = BASE_URL_ENV || 'http://10.40.31.51/UTEPSA-Eventos/server/src';
 //export const BASE_URL = 'http://10.40.31.51/UTEPSA-Eventos/server/src';
-//export const BASE_URL = 'https://anghis.com/server/src';
+//export const BASE_URL = 'https://anghis.com/src';
